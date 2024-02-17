ba268e4d8ebdbba73512cdc5afb2fce1d4fbcd33
Satvik <satvik.pss@gmail.com>
2024-02-17 09:22:00 +0530

    first commit

---
+++ chatbot.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from streamlit_chat import message
+# from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+
+import os
+
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+
+# Initialize session state variables
+if 'buffer_memory' not in st.session_state:
+    st.session_state.buffer_memory = ConversationBufferWindowMemory(k=3, return_messages=True)
+
+if "messages" not in st.session_state.keys(): # Initialize the chat message history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "How can I help you today?"}
+    ]
+
+# Initialize ChatOpenAI and ConversationChain
+llm = ChatOpenAI(model_name="gpt-3.5-turbo-0125")
+# llm = ChatGoogleGenerativeAI(model = "gemini-pro")
+
+conversation = ConversationChain(memory=st.session_state.buffer_memory, llm=llm)
+
+# Create user interface
+st.title("🗣️ Conversational Chatbot")
+st.subheader("㈻ Simple Chat Interface for LLMs")
+
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = conversation.predict(input = prompt)
+            st.write(response)
+            message = {"role": "assistant", "content": response}
+            st.session_state.messages.append(message) # Add response to message history
