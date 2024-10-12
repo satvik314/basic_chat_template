ab866f67e11f18e6e6a9c981d4789b20d43b65ab
satvik314 <113601830+satvik314@users.noreply.github.com>
2024-10-12 20:14:39 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,13 +2,13 @@ import streamlit as st
 from streamlit_chat import message
 # from langchain.chat_models import ChatOpenAI
 from langchain_openai import ChatOpenAI
-from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains import ConversationChain
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 
 import os
 
-os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+# os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
 
 # Initialize session state variables
 if 'buffer_memory' not in st.session_state:
@@ -20,14 +20,19 @@ if "messages" not in st.session_state.keys(): # Initialize the chat message hist
     ]
 
 # Initialize ChatOpenAI and ConversationChain
-llm = ChatOpenAI(model_name="gpt-3.5-turbo-0125")
+# llm = ChatOpenAI(model_name="gpt-4o-mini")
 # llm = ChatGoogleGenerativeAI(model = "gemini-pro")
+lmm = ChatOpenAI(model = "meta-llama/Llama-3.2-90B-Vision-Instruct-Turbo",
+                      openai_api_key = st.secrets["TOGETHER_API_KEY"] , ## use your key
+                      openai_api_base = "https://api.together.xyz/v1"
+
+)
 
 conversation = ConversationChain(memory=st.session_state.buffer_memory, llm=llm)
 
 # Create user interface
 st.title("🗣️ Conversational Chatbot")
-st.subheader("㈻ Simple Chat Interface for LLMs")
+st.subheader("㈻ Simple Chat Interface for LLMs by Build Fast with AI")
 
 
 if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
